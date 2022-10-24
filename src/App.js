429a3c3a699ad135e4e683557d0448316693cf68
samsamgo <jku2659@gmail.com>
2022-10-25 00:20:28 +0900

    state
    component life cycle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   state = {
     count: 0
   };
   add = () => {
-    this.setState(current => { count: current.count + 1 })
+    this.setState(current => ({ count: current.count + 1 }))
   }
   minus = () => {
-    this.setState(current => { count: current.count - 1 })
+    this.setState(current => ({ count: current.count - 1 }))
+  }
+  componentDidMount() {
+    console.log("Im mount");
+  }
+  componentDidUpdate() {
+    console.log("Im update");
+  }
+  componentWillUnmount() {
+    console.log("bye this code end")
   }
 
   render() {
+    console.log("Im rander");
     return (
       <div>
         <h1>The number is:  {this.state.count}</h1>
