91f48087b9fe043c455d65539e02945bea5d607e
samsamgo <jku2659@gmail.com>
2022-10-26 23:54:08 +0900

    fetch
    fetch movie json file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true
   }
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    console.log(movies)
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false })
-    }, 6000)
+    this.getMovies();
+
   }
   render() {
     const { isLoading } = this.state
